bb41f065cd9dc1e6ca4235d3a0d70ba677f7f3de
Estela Cardalda <estela.cardalda.vidal@gmail.com>
2017-08-18 23:29:51 +0200

    Changes to deploy in Apache Tomcat

--- suhsport/src/main/java/com/escaravellovermello/SuhsportApplication.java
+++ suhsport/src/main/java/com/escaravellovermello/SuhsportApplication.java
@@ -2,11 +2,19 @@ package com.escaravellovermello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SuhsportApplication {
+public class SuhsportApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SuhsportApplication.class, args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SuhsportApplication.class);
+	}
+	
 }
